ea47461b83d57a1e79609a732955271893155310
Lothar123 <w.slubowska@gmail.com>
2022-04-27 21:51:37 +0200

    wer

---
+++ kriostat.pl/kriostat.pl/Pages/Graph.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kriostat.pl.Pages
+{
+    public class GraphModel : PageModel
+    {
+        private readonly ILogger<GraphModel> _logger;
+
+        public GraphModel(ILogger<GraphModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
