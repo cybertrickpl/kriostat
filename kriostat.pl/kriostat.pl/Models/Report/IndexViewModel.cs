0f5a02a6c4b60b744950391389e2f93501dbb1c7
Lothar123 <w.slubowska@gmail.com>
2022-04-28 15:12:03 +0200

    abc

---
+++ kriostat.pl/kriostat.pl/Models/Report/IndexViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace kriostat.pl.Models.Report
+{
+    public class IndexViewModel
+    {
+        public int Year { get; set; }
+        public string FirstName { get; set; }
+
+        public bool IsJacek
+        {
+            get
+            {
+                if (FirstName != null)
+                {
+                    if (FirstName.ToUpper() == "JACEK") { return true; }
+                }
+                
+                return false;
+            }
+        }
+
+        public double SqrtYear
+        {
+            get
+            {
+                return Math.Sqrt(Year);
+            }
+        }
+    }
+}
--- kriostat.pl/kriostat.pl/Program.cs
+++ kriostat.pl/kriostat.pl/Program.cs
@@ -20,5 +20,9 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 app.MapRazorPages();
+app.MapControllerRoute(
+    name:"default",
+    pattern:"{controller=Home}/{action=Index}/{id?}"
+    );
 
 app.Run();
