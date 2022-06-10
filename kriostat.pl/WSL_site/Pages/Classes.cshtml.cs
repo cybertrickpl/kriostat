db45e8a114475a98e16b198361af329bbfdc9e66
Lothar123 <w.slubowska@gmail.com>
2022-06-10 15:12:09 +0200

    new

---
+++ kriostat.pl/WSL_site/Pages/Classes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WSL_site.Pages
+{
+    public class ClassesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ kriostat.pl/WSL_site/Pages/research.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WSL_site.Pages
+{
+    public class researchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
