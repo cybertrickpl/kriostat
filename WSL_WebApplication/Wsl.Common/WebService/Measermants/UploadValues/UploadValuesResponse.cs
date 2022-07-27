fa91b313041c183f8d103875afafdb6b271de7dc
michalw <contact@cybertrick.pl>
2022-07-27 20:55:53 +0200

    Upload

---
+++ WSL_WebApplication/Wsl.Common/WebService/Measermants/UploadValues/UploadValuesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wsl.Common.Dto;
+
+namespace Wsl.Common.WebService.MaterialType.Add
+{
+    public class UploadValuesResponse
+    {
+        public int Id { get; set; }
+    }
+}
